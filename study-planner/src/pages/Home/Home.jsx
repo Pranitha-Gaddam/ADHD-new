a37f0ef52df91cba1f3b4c9a8b0421cd591647a2
KimVVo <thienkimvo0111@gmail.com>
2025-02-20 12:58:03 -0600

    create signup page

--- study-planner/src/pages/Home/Home.jsx
+++ study-planner/src/pages/Home/Home.jsx
@@ -1,14 +1,5 @@
-
-
 const Home = () => {
-  return (
-    <div>
-      <div className="bg-blue-500 p-2">Test</div>
-      <h1 className="text-1xl font-bold underline text-center">
-        Hello world!
-      </h1>
-    </div>
-  );
+  return <></>;
 };
 
 export default Home;
--- study-planner/src/pages/Login/Login.jsx
+++ study-planner/src/pages/Login/Login.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
-import { validateEmail } from '../../utils/helper';
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -21,44 +21,55 @@ const Login = () => {
       setError("Password must be at least 8 characters long.");
       return;
     }
-  }
+
+    if (!password) {
+      setError("Please enter your password.");
+      return;
+    }
+
+    // Login API call
+  };
   return (
     <>
       <Navbar />
 
       <div className="flex justify-center items-center mt-28">
-        <div className="w-96 border border-white py-10 rounded px-7"> 
+        <div className="w-96 border bg-white py-10 rounded px-7">
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
 
-            <input 
-              type="text" 
-              className="input-box" 
-              placeholder="Email" 
+            <input
+              type="text"
+              className="input-box"
+              placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
 
-            <PasswordInput 
+            <PasswordInput
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
 
             {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
-            <button type="submit" className="btn-primary">Login</button>
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
 
             <p className="text-sm-text-center mt-4">
               Not registered yet?{" "}
-              <Link to="/signUp" className="font-medium text-primary text-amber-300 underline">
+              <Link
+                to="/signUp"
+                className="font-medium text-primary text-blue-600 underline"
+              >
                 Create an account
               </Link>
-
             </p>
           </form>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Login;
