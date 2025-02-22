5ae5961331bea78dacdc5b811df1831487e989f6
KimVVo <thienkimvo0111@gmail.com>
2025-02-22 03:23:12 -0600

    add Login API

--- frontend/study-planner/src/pages/Login/Login.jsx
+++ frontend/study-planner/src/pages/Login/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -28,6 +30,30 @@ const Login = () => {
     }
 
     // Login API call
+    try {
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+      });
+
+      // Handle successful login response
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        // Redirect to home page
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      // Handle login error
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again later.");
+      }
+    }
   };
   return (
     <>
